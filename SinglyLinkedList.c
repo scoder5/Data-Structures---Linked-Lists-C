2cbef48a05a17794840280fa36394ad497833edd
RAGHAVENDRA SURYA T.R <88027374+scoder5@users.noreply.github.com>
2023-01-26 18:14:31 +0530

    new commit

--- SinglyLinkedList.c
+++ SinglyLinkedList.c
@@ -6,3 +6,23 @@ typedef struct node{
     struct NODE *next;
 }NODE;
 
+//Allocating memory to a node
+NODE *getNode(void){
+    NODE *temp;
+    temp = (NODE *)malloc(sizeof(NODE));
+    return temp;
+}
+
+//Freeing memory
+void freeNode(NODE *temp){
+    free(temp);
+}
+
+//Inserting a node at the beginning of a linked list
+NODE *ins_first(NODE *first, int data){
+    NODE *newnode;
+    newnode = (NODE *)malloc(sizeof(NODE));
+    newnode->info = data;
+    newnode->next = first;
+    return newnode;
+}

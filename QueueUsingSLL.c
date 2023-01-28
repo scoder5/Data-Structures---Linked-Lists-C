53540f5b9f8052b3c0ff4e9f5eeafbc049ce7a7c
RAGHAVENDRA SURYA T.R <88027374+scoder5@users.noreply.github.com>
2023-01-28 14:52:58 +0530

    new commit

---
+++ QueueUsingSLL.c
@@ -0,0 +1,6 @@
+//Queue using Singly Linked List
+#include<stdio.h>
+#include<stdlib.h>
+
+//Representation of a node in SLL
+//A structure containing a member which is a pointer to the same structure is called self referential structure.

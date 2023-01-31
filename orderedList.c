c9fff6b523952bb1e4cfc223845d4cbd362f1feb
RAGHAVENDRA SURYA T.R <88027374+scoder5@users.noreply.github.com>
2023-01-31 16:33:07 +0530

    new commit

---
+++ orderedList.c
@@ -0,0 +1,30 @@
+//A list in which the elements are arranged in some order is referred to as an ordered list. 
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct node{
+    int info;
+    struct NODE *next;
+}NODE;
+
+NODE *Insert(NODE *first, int data){
+    NODE *newnode, *temp, *prev;
+    newnode = (NODE *)malloc(sizeof(NODE));
+    newnode->info = data;
+    if(first == NULL || data < first->info){
+        newnode->next = first;
+        first = newnode;
+    }
+    else{
+        temp = first;
+        while(temp != NULL && data > temp->info){
+            prev = temp;
+            temp = temp->next;
+        }
+        if(temp == NULL || data != temp->info){
+            prev->next = newnode;
+            newnode->next = temp;
+        }
+    }
+    return first;
+}
